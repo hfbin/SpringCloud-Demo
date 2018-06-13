6c701d798bb1f2915c55769d63e9f176263fa304
hfbin <1716850375@qq.com>
2018-06-13 09:50:20 +0800

    Eureka SpringBootApplication

---
+++ microservicecloud-eureka-7001/src/main/java/cn/hfbin/springcloud/EurekaServer7001_App.java
@@ -0,0 +1,15 @@
+package cn.hfbin.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by: HuangFuBin
+ * Date: 2018/6/13
+ * Time: 9:48
+ * Such description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer7001_App {
+}

6536727186dbfa4edb381daa4ebecda1dde5ee6c
hfbin <1716850375@qq.com>
2018-06-15 10:07:49 +0800

    consumer-dept-80

--- microservicecloud-eureka-7001/src/main/java/cn/hfbin/springcloud/EurekaServer7001_App.java
+++ microservicecloud-eureka-7001/src/main/java/cn/hfbin/springcloud/EurekaServer7001_App.java
@@ -1,5 +1,6 @@
 package cn.hfbin.springcloud;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
@@ -12,4 +13,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServer7001_App {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EurekaServer7001_App.class, args);
+    }
 }

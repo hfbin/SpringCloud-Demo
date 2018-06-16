8a37f6fc2b07f3ec4b053990ee7677072b0763ae
hfbin <1716850375@qq.com>
2018-06-16 17:15:34 +0800

    provider-dept-8002

--- microservicecloud-consumer-dept-80/src/main/java/cn/hfbin/springcloud/DeptConsumer80_App.java
+++ microservicecloud-consumer-dept-80/src/main/java/cn/hfbin/springcloud/DeptConsumer80_App.java
@@ -2,12 +2,14 @@ package cn.hfbin.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
 //在启动该微服务的时候就能去加载我们的自定义Ribbon配置类，从而使配置生效
 //@RibbonClient(name="MICROSERVICECLOUD-DEPT",configuration=MySelfRule.class)
 //@RibbonClient(name="MICROSERVICECLOUD-DEPT",configuration=MySelfRule.class)
+@EnableEurekaClient
 public class DeptConsumer80_App
 {
 	public static void main(String[] args)

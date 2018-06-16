b3ba4540d0a294d09c600378ab02983631cc7775
hfbin <1716850375@qq.com>
2018-06-17 01:25:34 +0800

    updata

--- microservicecloud-provider-dept-8001/sql/db.sql
+++ microservicecloud-provider-dept-8001/sql/db.sql
@@ -1,6 +1,8 @@
 #init SQL
 
-create table dept
+CREATE SCHEMA `springclouddb01` DEFAULT CHARACTER SET utf8 ;
+
+create table springclouddb01.dept
 (
 	deptno bigint not null primary key auto_increment,
     dname varchar(60),
@@ -13,4 +15,4 @@ insert into dept(dname , db_source) values('财务部' , database());
 insert into dept(dname , db_source) values('市场部' , database());
 insert into dept(dname , db_source) values('运维部' , database());
 
-select * from dept;
+select * from springclouddb01.dept;

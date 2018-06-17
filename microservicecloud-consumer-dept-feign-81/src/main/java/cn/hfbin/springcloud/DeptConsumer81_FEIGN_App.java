c9745b1a46532a26d99230b84dcc30ef1941b63e
hfbin <1716850375@qq.com>
2018-06-17 15:27:53 +0800

    uodata

---
+++ microservicecloud-consumer-dept-feign-81/src/main/java/cn/hfbin/springcloud/DeptConsumer81_FEIGN_App.java
@@ -0,0 +1,20 @@
+package cn.hfbin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients(basePackages= {"cn.hfbin.springcloud"})
+@ComponentScan("cn.hfbin.springcloud")
+public class DeptConsumer81_FEIGN_App
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(DeptConsumer81_FEIGN_App.class, args);
+	}
+}
